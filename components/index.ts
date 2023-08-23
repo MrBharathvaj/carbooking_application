0ac8f3f86fa42ba600809ae0079073c748e5ef37
Bharathvaj T <bharathvajt.it2020@gmail.com>
2023-08-23 15:41:11 +0530

    commit

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import React from "react";
+import CarCard from "./CarCard";
+export { Hero, CustomButton, Navbar, Footer, CustomFilter, SearchBar, SearchManufacturer, CarCard }
+
